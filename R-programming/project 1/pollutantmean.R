cdadd1705c5a6f6782b1cabffcdf7c7a62f4562e
Rahul Ravindran <rahulravindran0108@gmail.com>
2015-05-18 12:14:09 +0530

    adding the right file

--- R-programming/project 1/pollutantmean.R
+++ R-programming/project 1/pollutantmean.R
@@ -1,27 +1,13 @@
-complete <- function(directory, id = 1:332) {
-  ## 'directory' is a character vector of length 1 indicating
-  ## the location of the CSV files
-  
-  ## 'id' is an integer vector indicating the monitor ID numbers
-  ## to be used
-  
-  ## Return a data frame of the form:
-  ## id nobs
-  ## 1  117
-  ## 2  1041
-  ## ...
-  ## where 'id' is the monitor ID number and 'nobs' is the
-  ## number of complete cases 
-  files <- list.files( path = directory )[id]
-  files <- files[!is.na(files)]
-  
-  id <- c()
-  nobs <- c() 
-  
-  for(f in 1:length(files)){
-    data <- read.csv( paste(directory, "/", files[f], sep="") )
-    id = c(id, as.integer(substring(files[f],0,3)) )                       # append to vector
-    nobs = c(nobs, nrow(na.omit(data))) # append to vector, num rows, omit NA
-  }
-  return( data.frame(id=id, nobs=nobs) )
+pollutants <- function(directory, pollutant, id) {
+    fileName <- paste(formatC(id, width = 3, flag = "0"), ".csv", sep = "")
+    filePath <- paste(directory, "/", fileName, sep = "")
+    frame <- read.csv(filePath)
+    pollutantData <- frame[[pollutant]]
+    pollutantData[!is.na(pollutantData)]
+}
+
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+    pollutantLists <- sapply(id, function(id) pollutants(directory, pollutant, id))
+    pollutants <- unlist(pollutantLists)
+    mean(pollutants)
 }
